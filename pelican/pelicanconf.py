ad145a36f1e5549ea39b7b29b7aadf1d6a7f263f
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-08-02 12:51:06 -0700

    use gollyx-rainbow-pelican-theme

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -19,7 +19,7 @@ else:
     SITENAME = u'GollyX'
 
 PATH = 'content'
-THEME = 'gollyx-toroidal-pelican-theme'
+THEME = 'gollyx-rainbow-pelican-theme'
 
 # Don't try to turn HTML files into pages
 READERS = {'html': None}
